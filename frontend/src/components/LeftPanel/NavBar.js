cec2da77ea4e85757b7da779de4702da799619f9
Dhairya Jain <Dhairya.Jain@contracted.pmi.com>
2023-04-13 18:38:20 +0530

    Add Material UI and array for navbar items

--- frontend/src/components/LeftPanel/NavBar.js
+++ frontend/src/components/LeftPanel/NavBar.js
@@ -1,6 +1,90 @@
 import React from "react";
-import { FaUserAlt } from "react-icons/fa";
+import { IconButton } from "@mui/material";
+import { Person } from "@mui/icons-material";
+
 const NavBar = () => {
+  // Adding all the necessary sections in an array
+  const navLinks = [
+    {
+      id: 1,
+      ariaLabel: "basics",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 2,
+      ariaLabel: "address",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 3,
+      ariaLabel: "socials",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 4,
+      ariaLabel: "education",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 5,
+      ariaLabel: "work-experience",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 6,
+      ariaLabel: "skills",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 7,
+      ariaLabel: "projects",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 8,
+      ariaLabel: "certifications",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 9,
+      ariaLabel: "awards",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 10,
+      ariaLabel: "interests",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 11,
+      ariaLabel: "languages",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 12,
+      ariaLabel: "publications",
+      href: "#",
+      icon: <Person />,
+    },
+    {
+      id: 13,
+      ariaLabel: "references",
+      href: "#",
+      icon: <Person />,
+    },
+  ];
+
   return (
     <div className="border-2 border-black w-20 flex flex-col items-center">
       {/* TODO: Need to prepare a logo and favicon */}
@@ -9,20 +93,11 @@ const NavBar = () => {
         {/* TODO:  Need to provide icons for following
         Basic details, address, social media, work ex, education, awards, publications, certifications, skills, languages, interests, projects, references */}
 
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
-        <FaUserAlt />
+        {navLinks.map(({ id, ariaLabel, icon }) => (
+          <IconButton key={id} aria-label={ariaLabel}>
+            {icon}
+          </IconButton>
+        ))}
       </div>
     </div>
   );

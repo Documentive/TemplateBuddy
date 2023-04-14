3fdfeb99a767934db0a1d60cb12ea9cee253ef24
Dhairya Jain <Dhairya.Jain@contracted.pmi.com>
2023-04-14 17:56:32 +0530

    Maintain a separate config file for the section links for navbar

--- frontend/src/components/LeftPanel/NavBar.js
+++ frontend/src/components/LeftPanel/NavBar.js
@@ -1,124 +1,15 @@
 import React from "react";
 import { IconButton, Tooltip, Fade } from "@mui/material";
-import {
-  Person,
-  Apartment,
-  Public,
-  Work,
-  School,
-  EmojiEvents,
-  AutoStories,
-  Redeem,
-  ShapeLine,
-  Language,
-  OutdoorGrill,
-  Groups,
-  Handshake,
-} from "@mui/icons-material";
+// Adding all the necessary sections in an array
+import { navLinks } from "../../config/navbarConstants";
 
 const NavBar = () => {
-  // Adding all the necessary sections in an array
-  const navLinks = [
-    {
-      id: 1,
-      title: "Basics",
-      ariaLabel: "basics",
-      href: "#",
-      icon: <Person />,
-    },
-    {
-      id: 2,
-      title: "Location",
-      ariaLabel: "location",
-      href: "#",
-      icon: <Apartment />,
-    },
-    {
-      id: 3,
-      title: "Socials",
-      ariaLabel: "socials",
-      href: "#",
-      icon: <Public />,
-    },
-    {
-      id: 4,
-      title: "Education",
-      ariaLabel: "education",
-      href: "#",
-      icon: <School />,
-    },
-    {
-      id: 5,
-      title: "Work Experience",
-      ariaLabel: "work-experience",
-      href: "#",
-      icon: <Work />,
-    },
-    {
-      id: 6,
-      title: "Projects",
-      ariaLabel: "projects",
-      href: "#",
-      icon: <Groups />,
-    },
-    {
-      id: 7,
-      title: "Skills",
-      ariaLabel: "skills",
-      href: "#",
-      icon: <ShapeLine />,
-    },
-    {
-      id: 8,
-      title: "Certifications",
-      ariaLabel: "certifications",
-      href: "#",
-      icon: <Redeem />,
-    },
-    {
-      id: 9,
-      title: "Awards",
-      ariaLabel: "awards",
-      href: "#",
-      icon: <EmojiEvents />,
-    },
-    {
-      id: 10,
-      title: "Interests",
-      ariaLabel: "interests",
-      href: "#",
-      icon: <OutdoorGrill />,
-    },
-    {
-      id: 11,
-      title: "Languages",
-      ariaLabel: "languages",
-      href: "#",
-      icon: <Language />,
-    },
-    {
-      id: 12,
-      title: "Publications",
-      ariaLabel: "publications",
-      href: "#",
-      icon: <AutoStories />,
-    },
-    {
-      id: 13,
-      title: "References",
-      ariaLabel: "references",
-      href: "#",
-      icon: <Handshake />,
-    },
-  ];
-
+  
   return (
     <div className="border-2 border-black w-20 flex flex-col items-center">
       {/* TODO: Need to prepare a logo and favicon */}
       <div className="w-full h-16 border-2 border-orange-300">Logo</div>
       <div className="h-4/6 my-auto flex flex-col justify-evenly items-center">
-        {/* TODO:  Need to provide icons for following
-        Basic details, address, social media, work ex, education, awards, publications, certifications, skills, languages, interests, projects, references */}
 
         {navLinks.map(({ id, title, ariaLabel, icon }) => (
           <Tooltip
